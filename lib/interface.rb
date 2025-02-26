faf94c3760e42f9349973d456a8e37ab6185390d
Augustin Maleka <augustin@jekyllandhide.co.za>
2025-02-26 20:18:47 +0200

    Added compute_name method and interface to get full name

---
+++ lib/interface.rb
@@ -0,0 +1,19 @@
+require_relative "compute_name"
+
+# TODO: ask for the first name
+# TODO: ask for the middle name
+# TODO: ask for the last name
+# TODO: Print a greeting to the user with the full name concatenated,
+#       something like "Hello, Boris Paillard"
+
+puts "What's your first name?"
+first_name = gets.chomp
+
+puts "What's your middle name?"
+middle_name = gets.chomp
+
+puts "What's your last name?"
+last_name = gets.chomp
+
+full_name = compute_name(first_name, middle_name, last_name)
+puts "Hello, #{full_name}!. your full name has got #{full_name.length} letters altogether, and today"
